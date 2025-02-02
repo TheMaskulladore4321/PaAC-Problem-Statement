cd3dbae3ff4971230b1613da4edce30c8cb6764e
TheMaskulladore4321 <maskulladore4321@gmail.com>
2025-02-02 21:22:28 +0530

    modified:   activities.css 	modified:   activities.html 	modified:   activities.js 	new file:   gallery.html 	new file:   image copy.png 	new file:   image.png 	modified:   index.html

--- activities.js
+++ activities.js
@@ -1,14 +1,12 @@
 new Swiper('.card-wrapper', {
     loop: true,
   
-    // Pagination bullets
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dybamicBullets: true
     },
-  
-    // Navigation arrows
+
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
